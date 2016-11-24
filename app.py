d6cdbc7570de862998737e087c4e8378814bec9a
Gabe <gabe@databaseguy.com>
2016-11-23 20:54:20 -0800

    Testing schema hook.

--- app.py
+++
@@ -1,9 +0,0 @@
-def application(env, start_response):
-  status = '200 OK'
-  output = b'Hello, World!'
-
-  response_headers = [('Content-type', 'text/plain'),
-                       ('Content-length', str(len(output)))]
-  start_response(status, response_headers)
-
-  return [output]
